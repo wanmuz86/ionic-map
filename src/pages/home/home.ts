1eb9c1e53c8af8d34b02379ad259a23ccfa240be
wan muzaffar Wan Hashim <wanmuz86@gmail.com>
2017-11-06 08:29:47 +0800

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,33 @@
+import { Component ,  ViewChild, ElementRef } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+declare var google;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+	@ViewChild('map') mapElement: ElementRef;
+  map: any;
+ 
+
+  constructor(public navCtrl: NavController) {
+  	this.loadMap();
+
+  }
+  loadMap(){
+ 
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+  }
+
+}
